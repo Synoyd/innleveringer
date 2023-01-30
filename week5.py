5dacfc6168c73f7405d9d170aa45660b39a64c43
Synoyd <ottar.hellesvik@gmail.com>
2023-01-30 19:32:59 +0100

    ferdig

--- week5.py
+++ week5.py
@@ -6,12 +6,15 @@ def calculateCircumference(radius) :
 
 # Task 1
 # Output "good" if isBanana is true
-isBanana = False
+print ("\n Task 1")
+isBanana = True
 if isBanana == True:
     print ('good')
 
 # Task 2
 # Output "monkey sad" if isBanana is false, but "good" if true
+print ("\n Task 2")
+isBanana = False
 if isBanana == True:
     print ('good')
 elif isBanana == False:
@@ -19,6 +22,8 @@ elif isBanana == False:
 
 # Task 3
 # Output "hmmm" if isBanana is not true or false, but "good" if true, and "monkey 
+print ("\n Task 3")
+isBanana = 1
 if isBanana == True:
     print ('good')
 elif isBanana == False:
@@ -29,17 +34,21 @@ else:
 
 # Task 4
 # if isBanana and isPrimate then output, "Eating that banana".
+print ("\n Task 4")
+isBanana = True
 isPrimate = True
 if isBanana & isPrimate == True :
     print ('Eating that banana')
 
 # Task 5
 # if isBanana or isPrimate then output, "Everything is fine"
+print ("\n Task 5")
 if isBanana or isPrimate == True:
     print ('Everything is fine')
 
 # Task 6
 # if not isBanana and not isPrimate then output, "Ooops."
+print ("\n Task 6")
 isBanana = isPrimate = False
 if not isBanana & isPrimate == True:
     print ('Ooops')
@@ -49,7 +58,13 @@ if not isBanana & isPrimate == True:
 # NB! 
 # * This will requier you to make judgments about what is god or bad.
 # * This will requier you to think about the safty of the code. 
+print ("\n Task 7")
 def shorten(name):
-  return name.split(" ")[0][0,3] + ". " + name.split(" ")[1]
+    firstname = name.split(" ")[0]
+    lastname = name.split(" ")[1]
+    firstNameShorted = firstname[0:3] + ". "
+    
+    nameShorted = (firstNameShorted + lastname)
+    return nameShorted
 ShortenedName = shorten("Christian Simonsen")
 print(ShortenedName)
