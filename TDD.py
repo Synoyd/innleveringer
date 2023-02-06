71aa1a79535e14dc06deeb0c008e3e7a50d48515
Synoyd <ottar.hellesvik@gmail.com>
2023-02-06 23:37:32 +0100

    nice

--- TDD.py
+++ TDD.py
@@ -1,3 +1,39 @@
+import decimal
+
+
+inches = float(input("inches: "))
+unit = input("unit: ")
+mmToInches = 25.4
+cmToInches = 2.54
+mToInches = 0.0254
+numberOfInches = inches
+
+def convertInchestoMM(numberOfInches, mm):
+    return numberOfInches * mm
+
+def convertInchestoCM(numberOfInches, cm):
+    return numberOfInches * cm
+
+def convertInchestoM(numberOfInches, m):
+    return numberOfInches * m
+
+
+def test(numberOfInches):
+    if(unit == "-mm"):
+        print(convertInchestoMM(numberOfInches, mmToInches))
+
+    elif(unit == "-m"):     
+        print(convertInchestoM(numberOfInches, mToInches))
+
+    elif(unit == "-cm"):
+        print(convertInchestoCM(numberOfInches, cmToInches))
+        
+    else:
+        print("Error: ukjent enhet")
+
+
+print(test(numberOfInches))
+
 #måten formen det skal være på er
 #output = funksjon() 
 #expected output = KONKRET VERDI
@@ -6,10 +42,21 @@
 #else
 #   print ("feil")
 
-inch = 2
-cm = 2.54
+successmsg = ('Grønn: Suksess det funker fint')
+errormsg = ('Rød: Det funker ikkje prøv igjen')
 
-if inch * 2.54 == cm :
-    print ('correct')
-else :
-    print ('wrong')
+
+if convertInchestoCM(1, mmToInches) == cmToInches:
+    print(successmsg)
+else:
+    print(errormsg)
+
+if convertInchestoM(1, mmToInches) == mToInches:
+    print(successmsg)
+else:
+    print(errormsg)
+
+if convertInchestoMM(1, mmToInches) == mmToInches:
+    print(successmsg)
+else:
+    print(errormsg)
